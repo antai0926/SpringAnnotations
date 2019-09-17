c1596be4573fef2c16fa156e68691feb66914c3f
antai0926 <antai0926@gmail.com>
2019-09-17 23:00:53 +0800

    use Field Injection

--- spring-demo-annotations/src/com/antai/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/antai/springdemo/TennisCoach.java
@@ -5,30 +5,25 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
-	private FortuneService fortuneService;
 	
+	@Autowired
+	private FortuneService fortuneService;
 	
 	//define a default constructor
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
 	}
 	
-	/*
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
-	}*/
-	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
 	}
 
-	@Autowired
-	public void doSomeCrazyStuff(FortuneService theFortuneService) {
-		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
-		this.fortuneService = theFortuneService;
-	}
+//	@Autowired
+//	public void doSomeCrazyStuff(FortuneService theFortuneService) {
+//		System.out.println(">> TennisCoach: inside doSomeCrazyStuff() method");
+//		this.fortuneService = theFortuneService;
+//	}
 
 	@Override
 	public String getDailyFortune() {
