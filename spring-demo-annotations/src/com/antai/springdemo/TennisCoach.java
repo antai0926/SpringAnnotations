86b7bc08ad95a6410e88c2ef2394d9a2c6486b40
antai0926 <antai0926@gmail.com>
2019-09-17 18:36:02 +0800

    use setter injection with annotation

--- spring-demo-annotations/src/com/antai/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/antai/springdemo/TennisCoach.java
@@ -7,14 +7,28 @@ import org.springframework.stereotype.Component;
 public class TennisCoach implements Coach {
 	private FortuneService fortuneService;
 	
+	
+	//define a default constructor
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	/*
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
-	}
+	}*/
 	
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
+	}
+
+	@Autowired
+	//define a setter method
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println(">> TennisCoach: inside setFortuneService() method");
+		this.fortuneService = theFortuneService;
 	}
 
 	@Override
