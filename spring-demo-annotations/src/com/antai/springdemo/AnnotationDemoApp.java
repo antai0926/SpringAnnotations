253b03a47d84ee4fc9fe04b6d56af23898b6ae99
antai0926 <antai0926@gmail.com>
2019-09-24 21:09:29 +0800

    inject properties file using java annotations

--- spring-demo-annotations/src/com/antai/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/antai/springdemo/AnnotationDemoApp.java
@@ -12,11 +12,14 @@ public class AnnotationDemoApp {
 		//get the bean from spring container
 		Coach theTennisCoach = context.getBean("tennisCoach", Coach.class);
 		Coach theBaseballCoach = context.getBean("baseballCoach", Coach.class);
+		SwimCoach theSwimCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		//call a method on the bean
 		System.out.println(theTennisCoach.getDailyWorkout());
 		System.out.println(theTennisCoach.getDailyFortune());
 		System.out.println(theBaseballCoach.getDailyWorkout());
+		
+		System.out.println("Email="+theSwimCoach.getEmail()+";The Team="+theSwimCoach.getTeam());
 		
 		//close the context
 		context.close();
